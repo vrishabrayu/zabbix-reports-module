972bbe489876f460ea9491831d0fa6ce3107d4ba
vrishabrayu <vrishabrayu@github.com>
2026-06-14 11:52:23 +0000

    Fix: rewrite view using native Zabbix CWidget/CTableInfo components like X-Mars modules

--- actions/CControllerCustomReportsView.php
+++ actions/CControllerCustomReportsView.php
@@ -8,14 +8,11 @@ class CControllerCustomReportsView extends CController {
 
 	protected function checkInput(): bool {
 		$fields = [
-			'from'       => 'string',
-			'to'         => 'string',
-			'groupid'    => 'id',
-			'filter_set' => 'in 1'
+			'from'    => 'string',
+			'to'      => 'string',
+			'groupid' => 'id'
 		];
-
-		$ret = $this->validateInput($fields);
-		return $ret;
+		return $this->validateInput($fields);
 	}
 
 	protected function checkPermissions(): bool {
@@ -23,22 +20,20 @@ class CControllerCustomReportsView extends CController {
 	}
 
 	protected function doAction(): void {
-		$from = $this->getInput('from', date('Y-m-d', strtotime('-30 days')));
-		$to   = $this->getInput('to', date('Y-m-d'));
+		$from    = $this->getInput('from', date('Y-m-d', strtotime('-30 days')));
+		$to      = $this->getInput('to', date('Y-m-d'));
 		$groupid = $this->getInput('groupid', 0);
 
-		// Get host groups for filter dropdown
 		$groups = API::HostGroup()->get([
-			'output' => ['groupid', 'name'],
+			'output'    => ['groupid', 'name'],
 			'sortfield' => 'name'
 		]);
 
-		// Build host filter
 		$host_filter = [
 			'output'           => ['hostid', 'host', 'name', 'status'],
 			'selectInterfaces' => ['ip'],
 			'selectGroups'     => ['name'],
-			'selectInventory'  => ['os', 'type', 'location'],
+			'selectInventory'  => ['os', 'type'],
 			'monitored_hosts'  => true,
 			'sortfield'        => 'name'
 		];
@@ -46,114 +41,70 @@ class CControllerCustomReportsView extends CController {
 			$host_filter['groupids'] = [$groupid];
 		}
 
-		$hosts = API::Host()->get($host_filter);
-		$host_data = [];
-
-		$time_from = strtotime($from . ' 00:00:00');
-		$time_to   = strtotime($to . ' 23:59:59');
+		$hosts         = API::Host()->get($host_filter);
+		$time_from     = strtotime($from . ' 00:00:00');
+		$time_to       = strtotime($to . ' 23:59:59');
 		$total_seconds = $time_to - $time_from;
+		$host_data     = [];
 
 		foreach ($hosts as $host) {
-			$hostid = $host['hostid'];
-			$ip = '';
-			if (!empty($host['interfaces'])) {
-				$ip = $host['interfaces'][0]['ip'];
-			}
+			$hostid     = $host['hostid'];
+			$ip         = !empty($host['interfaces']) ? $host['interfaces'][0]['ip'] : 'N/A';
 			$group_name = !empty($host['groups']) ? $host['groups'][0]['name'] : 'N/A';
 
-			// Get CPU utilization item
-			$cpu_items = API::Item()->get([
-				'output'  => ['lastvalue', 'units'],
-				'hostids' => [$hostid],
-				'search'  => ['key_' => 'system.cpu.util'],
-				'limit'   => 1
-			]);
-			$cpu_util = (!empty($cpu_items)) ? round((float)$cpu_items[0]['lastvalue'], 2) . '%' : 'N/A';
+			// CPU
+			$cpu_items = API::Item()->get(['output' => ['lastvalue'], 'hostids' => [$hostid], 'search' => ['key_' => 'system.cpu.util'], 'limit' => 1]);
+			$cpu_util  = !empty($cpu_items) ? round((float)$cpu_items[0]['lastvalue'], 2) . '%' : 'N/A';
 
-			// Get memory utilization item
-			$mem_items = API::Item()->get([
-				'output'  => ['lastvalue', 'units'],
-				'hostids' => [$hostid],
-				'search'  => ['key_' => 'vm.memory.utilization'],
-				'limit'   => 1
-			]);
-			if (empty($mem_items)) {
-				$mem_items = API::Item()->get([
-					'output'  => ['lastvalue'],
-					'hostids' => [$hostid],
-					'search'  => ['key_' => 'mem.util.used'],
-					'limit'   => 1
-				]);
-			}
-			$mem_util = (!empty($mem_items)) ? round((float)$mem_items[0]['lastvalue'], 2) . '%' : 'N/A';
+			// Memory
+			$mem_items = API::Item()->get(['output' => ['lastvalue'], 'hostids' => [$hostid], 'search' => ['key_' => 'vm.memory.utilization'], 'limit' => 1]);
+			$mem_util  = !empty($mem_items) ? round((float)$mem_items[0]['lastvalue'], 2) . '%' : 'N/A';
 
-			// Get uptime item
-			$uptime_items = API::Item()->get([
-				'output'  => ['lastvalue'],
-				'hostids' => [$hostid],
-				'search'  => ['key_' => 'system.uptime'],
-				'limit'   => 1
-			]);
-			if (!empty($uptime_items)) {
-				$uptime_sec = (int)$uptime_items[0]['lastvalue'];
-				$days    = floor($uptime_sec / 86400);
-				$hours   = floor(($uptime_sec % 86400) / 3600);
-				$uptime  = "{$days}d {$hours}h";
+			// Uptime
+			$up_items = API::Item()->get(['output' => ['lastvalue'], 'hostids' => [$hostid], 'search' => ['key_' => 'system.uptime'], 'limit' => 1]);
+			if (!empty($up_items)) {
+				$s      = (int)$up_items[0]['lastvalue'];
+				$uptime = floor($s / 86400) . 'd ' . floor(($s % 86400) / 3600) . 'h';
 			} else {
 				$uptime = 'N/A';
 			}
 
-			// Calculate SLA (availability) from problems in date range
-			$problems = API::Problem()->get([
-				'output'     => ['eventid', 'clock', 'r_clock'],
-				'hostids'    => [$hostid],
-				'time_from'  => $time_from,
-				'time_till'  => $time_to,
-				'recent'     => false
-			]);
-
+			// SLA
+			$problems         = API::Problem()->get(['output' => ['clock', 'r_clock'], 'hostids' => [$hostid], 'time_from' => $time_from, 'time_till' => $time_to, 'recent' => false]);
 			$downtime_seconds = 0;
-			foreach ($problems as $problem) {
-				$p_start = max((int)$problem['clock'], $time_from);
-				$p_end   = ($problem['r_clock'] > 0)
-					? min((int)$problem['r_clock'], $time_to)
-					: $time_to;
-				if ($p_end > $p_start) {
-					$downtime_seconds += ($p_end - $p_start);
-				}
+			foreach ($problems as $p) {
+				$ps = max((int)$p['clock'], $time_from);
+				$pe = ($p['r_clock'] > 0) ? min((int)$p['r_clock'], $time_to) : $time_to;
+				if ($pe > $ps) $downtime_seconds += ($pe - $ps);
 			}
-
-			$sla = ($total_seconds > 0)
-				? round((($total_seconds - $downtime_seconds) / $total_seconds) * 100, 3)
-				: 100;
-
+			$sla    = ($total_seconds > 0) ? round((($total_seconds - $downtime_seconds) / $total_seconds) * 100, 3) : 100;
 			$status = ($host['status'] == HOST_STATUS_MONITORED) ? 'Up' : 'Down';
 
 			$host_data[] = [
-				'name'        => $host['name'],
-				'host'        => $host['host'],
-				'ip'          => $ip,
-				'group'       => $group_name,
-				'os'          => $host['inventory']['os'] ?? 'N/A',
-				'type'        => $host['inventory']['type'] ?? 'N/A',
-				'status'      => $status,
-				'cpu_util'    => $cpu_util,
-				'mem_util'    => $mem_util,
-				'uptime'      => $uptime,
-				'sla'         => $sla . '%',
-				'sla_raw'     => $sla,
-				'problems'    => count($problems),
-				'downtime'    => gmdate('H:i:s', $downtime_seconds)
+				'name'     => $host['name'],
+				'host'     => $host['host'],
+				'ip'       => $ip,
+				'group'    => $group_name,
+				'os'       => $host['inventory']['os'] ?? 'N/A',
+				'type'     => $host['inventory']['type'] ?? 'N/A',
+				'status'   => $status,
+				'cpu_util' => $cpu_util,
+				'mem_util' => $mem_util,
+				'uptime'   => $uptime,
+				'sla'      => $sla . '%',
+				'sla_raw'  => $sla,
+				'problems' => count($problems),
+				'downtime' => gmdate('H:i:s', $downtime_seconds)
 			];
 		}
 
 		$this->setResponse(new CControllerResponseData([
-			'host_data'  => $host_data,
-			'groups'     => $groups,
-			'from'       => $from,
-			'to'         => $to,
-			'groupid'    => $groupid,
-			'title'      => 'Device & SLA Report'
+			'host_data' => $host_data,
+			'groups'    => $groups,
+			'from'      => $from,
+			'to'        => $to,
+			'groupid'   => $groupid,
+			'title'     => 'Device & SLA Report'
 		]));
 	}
 }
